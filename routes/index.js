a381632fee03e85624233738386f93b27b3bb0d3
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-21 23:13:31 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(process.env.DBPATH);
+require('dotenv').config();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* GET data from DB */
+
+router.get('/dbfacts', function (req, res, next) {
+  db.all("SELECT * FROM facts", function (err, rows) {
+    res.json(rows);
+  });
 });
 
 module.exports = router;
