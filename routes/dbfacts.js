7cd1e4056b8df6b055e527c3013071cdc78f9e0e
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-21 23:12:13 -0400

    Initial commit

---
+++ routes/dbfacts.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(process.env.DBPATH);
+require('dotenv').config();
+
+
+/* GET data from DB */
+
+router.get('/dbfacts', function (req, res, next) {
+    db.all("SELECT * FROM facts", function (err, rows) {
+        res.json(rows);
+    });
+});
+
+module.exports = router;
