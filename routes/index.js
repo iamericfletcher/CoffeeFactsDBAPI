4f5fff0b4e2f78cefea42187aec769a0d79e2ae6
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-21 23:56:04 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database("/home/iamericfletcher/coffeefacts.db");
 
+console.log("DB opened");
 /* GET data from DB */
 
-router.get('/dbfacts', function (req, res, next) {
+router.get('/', function (req, res, next) {
   db.all("SELECT * FROM facts", function (err, rows) {
     res.json(rows);
   });
