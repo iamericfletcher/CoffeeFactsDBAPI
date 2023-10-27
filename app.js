8f4b5d6132b651e951b892640fd71291ffa4ed56
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-27 19:58:31 -0400

    Update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/facts');
 
 var app = express();
 
@@ -15,6 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/facts', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,46 @@
 var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
+const { auth } = require('express-oauth2-jwt-bearer');
+require('dotenv').config();
+
+// Initialize SQLite database
 // let db = new sqlite3.Database("/Users/ericfletcher/coffeefacts.db");
-// let db = new sqlite3.Database("~/coffeefacts.db");
+// Uncomment the line below for a different database path
+// let db = new sqlite3.Database("/home/iamericfletcher/coffeefacts.db");
+let db = new sqlite3.Database(process.env.DBPATH);
 
-/* GET data from DB */
+// Configure JWT authentication settings
+const jwtCheck = auth({
+    audience: 'http://localhost:3002',
+    issuerBaseURL: 'https://dev-zrsam7livd1kfvrr.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+});
 
-// router.get('/', function (req, res, next) {
-//   db.all("SELECT * FROM facts", function (err, rows) {
-//     res.json(rows);
-//   });
-// });
+// Public route to GET facts from database
+router.get('/public', function (req, res, next) {
+    // SQL query to fetch required columns
+    db.all("SELECT id, fact, source, submitted_on FROM facts", function (err, rows) {
+        if (err) {
+            // Handle errors and send a 500 status
+            return res.status(500).json({"error": err.message});
+        }
+        // Send fetched rows as JSON response
+        res.json(rows);
+    });
+});
+
+// Private route to GET facts, with JWT authentication
+router.get('/private', jwtCheck, function (req, res, next) {
+    // SQL query to fetch all columns
+    db.all("SELECT * FROM facts", function (err, rows) {
+        if (err) {
+            // Handle errors and send a 500 status
+            return res.status(500).json({"error": err.message});
+        }
+        // Send fetched rows as JSON response
+        res.json(rows);
+    });
+});
 
 module.exports = router;
