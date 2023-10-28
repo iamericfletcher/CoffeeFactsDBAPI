2067cc1ec4e8ea4bb9d2f680450255d7ccf33499
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-28 16:56:59 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
-const { auth } = require('express-oauth2-jwt-bearer');
-let pingCount = 0;
+const {auth} = require('express-oauth2-jwt-bearer');
 require('dotenv').config();
 
 let db = new sqlite3.Database(process.env.DBPATH);
 // let db = new sqlite3.Database(process.env.TESTDBPATH);
+
+let routeCountDB = new sqlite3.Database(process.env.DBPATHROUTECOUNTS); // New SQLite database for route counts
+
+// Middleware to count route usage
+function countRoutes(req, res, next) {
+    const route = req.path;
+
+    // Increment counter in database
+    routeCountDB.run("INSERT OR IGNORE INTO routeCounts (route_name, access_count) VALUES (?, 0)", [route], function (err) {
+        if (err) return console.log(err);
+
+        routeCountDB.run("UPDATE routeCounts SET access_count = access_count + 1 WHERE route_name = ?", [route], function (err) {
+            if (err) return console.log(err);
+        });
+    });
+
+    next();
+}
 
 const jwtCheck = auth({
     audience: 'http://localhost:3002',
@@ -14,11 +31,7 @@ const jwtCheck = auth({
     tokenSigningAlg: 'RS256'
 });
 
-// Silly counter
-router.get('/ping',(req, res) => {
-    pingCount++;
-    res.send(`ping world for ${pingCount} times`);
-});
+router.use(countRoutes); // Apply the route count middleware to all routes
 
 router.get('/public', function (req, res) {
     db.all("SELECT id, fact, source, submitted_on FROM facts", function (err, rows) {
@@ -39,23 +52,23 @@ router.get('/private', jwtCheck, function (req, res) {
 });
 
 router.post('/addFact', jwtCheck, function (req, res) {
-    const { fact, source, user_id } = req.body;
+    const {fact, source, user_id} = req.body;
     const sql = `INSERT INTO facts (fact, source, user_id) VALUES (?, ?, ?)`;
     db.run(sql, [fact, source, user_id], function (err) {
         if (err) {
             return res.status(500).json({"error": err.message});
         }
-        return res.status(201).json({ "id": this.lastID });
+        return res.status(201).json({"id": this.lastID});
     });
 });
 
 // PUT route to edit a fact by ID
-router.put('/editFact/:id', jwtCheck, function(req, res) {
+router.put('/editFact/:id', jwtCheck, function (req, res) {
     const id = req.params.id;
-    const { fact, source } = req.body;
+    const {fact, source} = req.body;
     const sql = "UPDATE facts SET fact = ?, source = ? WHERE id = ?";
 
-    db.run(sql, [fact, source, id], function(err) {
+    db.run(sql, [fact, source, id], function (err) {
         if (err) {
             return res.status(500).json({"error": err.message});
         }
@@ -87,7 +100,6 @@ router.delete('/deleteFact/:id', jwtCheck, function (req, res) {
         return res.status(200).json({"id": id});
     });
 });
-
 
 
 module.exports = router;
