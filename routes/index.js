9b861b6a00c17d3bd542b8599e0d53241829306f
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-28 07:50:24 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,86 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const { auth } = require('express-oauth2-jwt-bearer');
 require('dotenv').config();
 
-// Initialize SQLite database
-// let db = new sqlite3.Database("/Users/ericfletcher/coffeefacts.db");
-// Uncomment the line below for a different database path
-// let db = new sqlite3.Database("/home/iamericfletcher/coffeefacts.db");
-let db = new sqlite3.Database(process.env.DBPATH);
+// let db = new sqlite3.Database(process.env.DBPATH);
+let db = new sqlite3.Database(process.env.TESTDBPATH);
 
-// Configure JWT authentication settings
 const jwtCheck = auth({
     audience: 'http://localhost:3002',
     issuerBaseURL: 'https://dev-zrsam7livd1kfvrr.us.auth0.com/',
     tokenSigningAlg: 'RS256'
 });
 
-// Public route to GET facts from database
-router.get('/public', function (req, res, next) {
-    // SQL query to fetch required columns
+router.get('/public', function (req, res) {
     db.all("SELECT id, fact, source, submitted_on FROM facts", function (err, rows) {
         if (err) {
-            // Handle errors and send a 500 status
             return res.status(500).json({"error": err.message});
         }
-        // Send fetched rows as JSON response
         res.json(rows);
     });
 });
 
-// Private route to GET facts, with JWT authentication
-router.get('/private', jwtCheck, function (req, res, next) {
-    // SQL query to fetch all columns
+router.get('/private', jwtCheck, function (req, res) {
     db.all("SELECT * FROM facts", function (err, rows) {
         if (err) {
-            // Handle errors and send a 500 status
             return res.status(500).json({"error": err.message});
         }
-        // Send fetched rows as JSON response
         res.json(rows);
     });
 });
+
+router.post('/addFact', jwtCheck, function (req, res) {
+    const { fact, source, user_id } = req.body;
+    const sql = `INSERT INTO facts (fact, source, user_id) VALUES (?, ?, ?)`;
+    db.run(sql, [fact, source, user_id], function (err) {
+        if (err) {
+            return res.status(500).json({"error": err.message});
+        }
+        return res.status(201).json({ "id": this.lastID });
+    });
+});
+
+// PUT route to edit a fact by ID
+router.put('/editFact/:id', jwtCheck, function(req, res) {
+    const id = req.params.id;
+    const { fact, source } = req.body;
+    const sql = "UPDATE facts SET fact = ?, source = ? WHERE id = ?";
+
+    db.run(sql, [fact, source, id], function(err) {
+        if (err) {
+            return res.status(500).json({"error": err.message});
+        }
+        res.json({"success": true});
+    });
+});
+
+
+// GET route to fetch a fact by ID for editing
+router.get('/editFact/:id', jwtCheck, function (req, res) {
+    const id = req.params.id;
+    const sql = "SELECT * FROM facts WHERE id = ?";
+
+    db.get(sql, [id], function (err, row) {
+        if (err) {
+            return res.status(500).json({"error": err.message});
+        }
+        res.json(row);
+    });
+});
+
+router.delete('/deleteFact/:id', jwtCheck, function (req, res) {
+    const id = req.params.id;
+    const sql = `DELETE FROM facts WHERE id = ?`;
+    db.run(sql, id, function (err) {
+        if (err) {
+            return res.status(500).json({"error": err.message});
+        }
+        return res.status(200).json({"id": id});
+    });
+});
+
+
 
 module.exports = router;
