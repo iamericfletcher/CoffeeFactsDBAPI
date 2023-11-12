f9b04f9178421e55e06c64743ea9683454b16139
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-11-12 16:19:17 -0500

    Update

--- routes/index.js
+++ routes/index.js
@@ -17,24 +17,24 @@ const limiter = rateLimit({
 router.use(limiter);
 
 // Middleware to count route usage
-// function countRoutes(req, res, next) {
-//     let route = req.path;
-//
-//     // If route includes '/editFact/' or '/deleteFact/', trim off the '/:id' portion
-//     if (route.includes('/editFact/') || route.includes('/deleteFact/')) {
-//         route = '/' + route.split('/')[1];
-//     }
-//
-//     // Increment counter in database
-//     db.run("INSERT OR IGNORE INTO routeCounts (route_name, access_count) VALUES (?, 0)", [route], function (err) {
-//         if (err) return console.log(err);
-//
-//         db.run("UPDATE routeCounts SET access_count = access_count + 1 WHERE route_name = ?", [route], function (err) {
-//             if (err) return console.log(err);
-//         });
-//     });
-//     next();
-// }
+function countRoutes(req, res, next) {
+    let route = req.path;
+
+    // If route includes '/editFact/' or '/deleteFact/', trim off the '/:id' portion
+    if (route.includes('/editFact/') || route.includes('/deleteFact/')) {
+        route = '/' + route.split('/')[1];
+    }
+
+    // Increment counter in database
+    db.run("INSERT OR IGNORE INTO routeCounts (route_name, access_count) VALUES (?, 0)", [route], function (err) {
+        if (err) return console.log(err);
+
+        db.run("UPDATE routeCounts SET access_count = access_count + 1 WHERE route_name = ?", [route], function (err) {
+            if (err) return console.log(err);
+        });
+    });
+    next();
+}
 
 // Middleware to check for valid JWT
 const jwtCheck = auth({
@@ -43,7 +43,8 @@ const jwtCheck = auth({
     tokenSigningAlg: process.env.TOKENSIGNINGALG
 });
 
-// router.use(countRoutes); // Apply the route count middleware to all routes
+
+router.use(countRoutes); // Apply the route count middleware to all routes
 
 router.get('/public', function (req, res) {
     db.all("SELECT id, fact, source, submitted_on FROM facts WHERE is_approved = 1", function (err, rows) {
@@ -67,7 +68,8 @@ router.get('/private', jwtCheck, function (req, res) {
 
 router.post('/addFact', jwtCheck, function (req, res) {
     const {fact, source, user_id} = req.body;
-    const sql = `INSERT INTO facts (fact, source, user_id) VALUES (?, ?, ?)`;
+    const sql = `INSERT INTO facts (fact, source, user_id)
+                 VALUES (?, ?, ?)`;
     db.run(sql, [fact, source, user_id], function (err) {
         if (err) {
             return res.status(500).json({"error": err.message});
@@ -111,9 +113,10 @@ router.delete('/deleteFact/:id', jwtCheck, function (req, res) {
         if (err) {
             return res.status(500).json({"error": err.message});
         }
-        return res.status(200).json({"id": id});
+        return res.status(200).json({"id": id, "user_id": req.user_id});
     });
 });
+
 
 // Route for getting all unapproved facts from the database for the admin panel on the frontend
 router.get('/unapprovedFacts', jwtCheck, function (req, res) {
@@ -126,8 +129,6 @@ router.get('/unapprovedFacts', jwtCheck, function (req, res) {
         res.json(rows);
     });
 });
-
-//TODO - add column for admin decision with timestamp for admin approval
 
 // Route for approving a fact by ID
 router.put('/adminApproveFact/:id', jwtCheck, function (req, res) {
@@ -142,15 +143,9 @@ router.put('/adminApproveFact/:id', jwtCheck, function (req, res) {
     });
 });
 
-//TODO - add column for admin decision with timestamp for admin reject & review
-
 // Route for deleting a fact by ID
 router.delete('/adminRejectFact/:id', jwtCheck, function (req, res) {
     const id = req.params.id;
-    // let instead set the is_approved to 2 instead of deleting it so the user can see that it was rejected
-    // and can edit it and resubmit it for approval within a certain time frame before it is deleted from the database
-    // admin rejected facts will be displayed in the user profile page under the admin rejected section
-    // admins would be required to provide a brief reason for rejecting the fact which would be displayed to the user
     const sql = "UPDATE facts SET is_approved = 2, admin_rejected_for_review_on = DATETIME() WHERE id = ?";
     // const sql = `DELETE FROM facts WHERE id = ?`;
     db.run(sql, id, function (err) {
@@ -164,12 +159,14 @@ router.delete('/adminRejectFact/:id', jwtCheck, function (req, res) {
 // Router for admin to delete a fact by ID
 router.delete('/adminDeleteFact/:id', jwtCheck, function (req, res) {
     const id = req.params.id;
-    const sql = `DELETE FROM facts WHERE id = ?`;
+    const sql = `DELETE
+                 FROM facts
+                 WHERE id = ?`;
     db.run(sql, id, function (err) {
         if (err) {
             return res.status(500).json({"error": err.message});
         }
-        return res.status(200).json({"id": id});
+        res.json({"success": true});
     });
 });
 
